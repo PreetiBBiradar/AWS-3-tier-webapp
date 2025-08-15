7671e0a75a060c9e06b7222e636888c8e00e8849
PreetiBBiradar <preetibb04@gmail.com>
2025-08-16 04:16:39 +0530

    added aws 3-tier files

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cbukc8ak4hzf.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root1995',
+    DB_DATABASE : 'webappdb'
+});
